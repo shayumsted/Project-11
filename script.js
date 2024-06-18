fa6ef1286680aa306fc770bd488a1f596f93ea48
Shamira Umstead <sumstead@usf.edu>
2024-06-18 12:30:30 -0400

    git commit -m "Configure linear scale for bar widths."

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ const barHeight = 20;
 const margin = 1;
 const height = data.length * (barHeight + margin);
 
+// Scaling lines
+const xScale = d3.scaleLinear()
+.domain([0, d3.max(data)])
+.range([50, width]);
+
+// Creating SVG Container
